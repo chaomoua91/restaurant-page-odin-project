5f353fe67c84b7f85f2dcf1ef00811defbf376e4
Chao Moua <chaomoua91@gmail.com>
2024-05-08 20:50:52 -0500

    easy text for right now but i like the background for the landing page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-
+import loadHome, { initialPage } from './pages/initial-page.js';
 import _ from 'lodash';
 import './style.css';
 
+
+loadHome();
+
+let tabSwitchingModule = (function() {
+    
+  const homeTab = document.querySelector(".home");
+  homeTab.addEventListener("click", initialPageLoad);
+
+  const menuTab = document.querySelector(".menu");
+  menuTab.addEventListener("click", menu);
+
+  const contactTab = document.querySelector(".contact");
+  contactTab.addEventListener("click", contact);
+
+})();
--- src/pages/initial-page.js
+++ src/pages/initial-page.js
@@ -1,1 +1,42 @@
-console.log("hello world")
+import '../style.css';
+import background from '../img/paper-lanturns.jpg';
+
+console.log("hello world");
+
+function loadHome() {
+    document.body.style.background = `url(${background})`
+    document.body.style.backgroundSize = "cover"
+
+    const contentDiv = document.querySelector("#content");
+
+    // Remove any existing DOM children if present
+    contentDiv.replaceChildren();
+
+    // DOM for heading
+    const heading = document.createElement("h1");
+    heading.textContent = "Welcome To Jaybees Tech Talk Restaurant And Lounge";
+    heading.classList.add("landing-title");
+    contentDiv.appendChild(heading);
+
+    // DOM for p tag #1
+    const para1 = document.createElement("p");
+    para1.classList.add("landing-page-copy");
+    para1.textContent = "This is an upscale restaurant that touts its elegance and spacious dining/lounge area.";
+    contentDiv.appendChild(para1);
+
+    // DOM for p tag #2
+    const para2 = document.createElement("p");
+    para2.classList.add("landing-page-copy");
+    para2.textContent = "Attire required for this facility is business casual at a minimum, please!";
+    contentDiv.appendChild(para2);
+
+    // DOM for p tag #3
+    const para3 = document.createElement("p");
+    para3.classList.add("landing-page-copy");
+    para3.textContent = "Thanks, Management";
+    contentDiv.appendChild(para3);
+  
+
+  }
+  
+  export default loadHome;
